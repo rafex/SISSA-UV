20881955af9e0e3666a0885392aefd892c520bb1
Raúl Eduardo González Argote <raulegleza@gmail.com>
2011-06-08 00:51:52 -0500

    modificando loggeo

--- index.php
+++ index.php
@@ -14,17 +14,25 @@ if($_SESSION['activa']){
 
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<title>
-Iniciar Sesion
-</title>
+
+<head>
+<!--meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" /-->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Author" content="Raul Eduargo Gonzalez Argote" />
+<meta name="Date" content="Feb 15, 2011" />
+<meta name="robots" content="all">
+<meta name="keywords" content="servicio,social,universitario,universidad,uv,veracruzana,xalapa,sistema,informacion" />
+<meta name="description" content="Sistema de Informacion del Departamento de Servicio Social de la Facultad de Contaduría y Administracion Campus Xalapa, Universidad Veracruzana. Administrando y Gestionando la información que se genera y recibe en este departamento." />
+
+
+<title>Iniciar Sesión en SISSA - UV </title>
 
 <!-- Meta Tags -->
-<meta charset="utf-8">
-<meta name="generator" content="Wufoo.com" />
+
+
 
 <!-- CSS -->
 <link rel="stylesheet" href="css/structure.css" type="text/css" />
